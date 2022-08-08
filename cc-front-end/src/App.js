409eed1644c4148c865694d81961ce9a93e407ef
akash18501 <akash24nov2000@gmail.com>
2022-08-08 14:19:09 +0530

    appbar and table added

--- cc-front-end/src/App.js
+++ cc-front-end/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import ResponsiveAppBar from "./Components/ResponsiveAppBar";
+import Table from "./Components/Table";
+import { Route, Routes } from "react-router-dom";
+import MyBook from "./Components/MyBook";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ResponsiveAppBar />
+      <Routes>
+        <Route exact path="/" element={<Table />} />
+        <Route exact path="/mybooks" element={<MyBook />} />
+      </Routes>
+    </>
   );
 }
 
